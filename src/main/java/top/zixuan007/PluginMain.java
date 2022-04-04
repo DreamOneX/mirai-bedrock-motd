974add6dffc533c12405b8794cf73b5d4a16a741
DreamOneX <1538874738@qq.com>
2022-04-05 01:22:04 +0800

    Update PluginMain.java

--- src/main/java/top/zixuan007/PluginMain.java
+++ src/main/java/top/zixuan007/PluginMain.java
@@ -45,7 +45,7 @@ public class PluginMain extends JavaPlugin {
         // 注册监听器
         Listener listener = GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessageEvent.class, event -> {
             String msg = event.getMessage().contentToString();
-            if (! msg.startsWith("#beping") {return;}
+            if (! msg.startsWith("#beping")) {return;}
                 
             String domain = msg.replace("#beping", "").toLowerCase().trim();
 
